47d854f8e0c7a97038ffc5e766173c2dc8adf2dc
Ralph Li <adad184@gmail.com>
2015-08-04 17:04:23 +0800

    resort the project file

--- MMParallaxCell/MMParallaxCell/AppDelegate.m
+++ MMParallaxCell/MMParallaxCell/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [ViewController new];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
